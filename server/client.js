5b3a3217882ffbeeaa942b516b110dec9446fd92
Kinnan Kwok <kkwoker@gmail.com>
2017-06-24 11:50:35 -0700

    move .gitignore ousside

--- server/client.js
+++ server/client.js
@@ -1,5 +1,5 @@
 const io = require('socket.io-client');
-const socket = io('http://localhost:3000');
+const socket = io('https://facematch-server.herokuapp.com/');
 
 socket.on('connect', () => {
   console.log('connected')
